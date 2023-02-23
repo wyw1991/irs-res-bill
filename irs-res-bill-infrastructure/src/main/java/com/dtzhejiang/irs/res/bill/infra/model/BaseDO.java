c990496edc56060bcabab5fecd7583622be9c667
wuyiwen <yiwen.wuyw@dtzhejiang.com>
2023-02-23 11:04:15 +0800

    生成代码

---
+++ irs-res-bill-infrastructure/src/main/java/com/dtzhejiang/irs/res/bill/infra/model/BaseDO.java
@@ -0,0 +1,28 @@
+package com.dtzhejiang.irs.res.bill.infra.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public abstract class BaseDO {
+
+    /**
+     * 主键id
+     */
+    @TableId(type = IdType.AUTO)
+    protected Long id;
+    /**
+     * 创建时间
+     */
+    protected Date createTime;
+    /**
+     * 更新时间
+     */
+    protected Date updateTime;
+
+}
