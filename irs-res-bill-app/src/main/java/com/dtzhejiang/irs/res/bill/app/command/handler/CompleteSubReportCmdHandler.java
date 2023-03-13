1bcc21868fbc6cff51573de98613e2cfafa6d8e9
zhangming <ming.zhangm@dtzhejiang.com>
2023-03-13 22:24:43 +0800

    feat: 更新历史处理人

--- irs-res-bill-app/src/main/java/com/dtzhejiang/irs/res/bill/app/command/handler/CompleteSubReportCmdHandler.java
+++ irs-res-bill-app/src/main/java/com/dtzhejiang/irs/res/bill/app/command/handler/CompleteSubReportCmdHandler.java
@@ -10,12 +10,14 @@ import com.dtzhejiang.irs.res.bill.domain.process.gateway.ProcessGateway;
 import com.dtzhejiang.irs.res.bill.domain.process.valueobject.ProcessTask;
 import com.dtzhejiang.irs.res.bill.domain.user.gateway.UserGateway;
 import com.dtzhejiang.irs.res.bill.infra.repository.SubReportRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class CompleteSubReportCmdHandler{
@@ -55,6 +57,11 @@ public class CompleteSubReportCmdHandler{
             throw new BusinessException("当前页面信息已失效,请刷新页面！");
         }
         processGateway.completeProcessTask(subReport.getTaskId(), variables, username);
+        String historyHandler = subReport.getHistoryHandler();
+        if(StringUtils.isNotBlank(historyHandler)){
+            historyHandler  = historyHandler + "," + username;
+        }
+        subReport.setHistoryHandler(historyHandler);
     }
 
 }
