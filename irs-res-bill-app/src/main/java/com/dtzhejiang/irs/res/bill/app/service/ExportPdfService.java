8337e3c24de910fe858380b1b8294a4d2cadaab4
wuyiwen <yiwen.wuyw@dtzhejiang.com>
2023-04-28 10:25:34 +0800

    加日志4

--- irs-res-bill-app/src/main/java/com/dtzhejiang/irs/res/bill/app/service/ExportPdfService.java
+++ irs-res-bill-app/src/main/java/com/dtzhejiang/irs/res/bill/app/service/ExportPdfService.java
@@ -37,11 +37,15 @@ public class ExportPdfService {
         //微软雅黑在windows系统里的位置如下，linux系统直接拷贝该文件放在linux目录下即可
         log.error("-------------------wyw:开始转化file==================");
         InputStream inputStreamPdf=new ByteArrayInputStream(WkHtmltoxPdfUtil.getPdfByte(file));
+        log.error("-------------------wyw:开始获取reportId==================");
+        Long reportId=Long.parseLong(file.getOriginalFilename().substring(0,file.getOriginalFilename().lastIndexOf(".")));
+        log.error("-------------------wyw:开始获取reportName==================");
+        String name=reportService.getReport(reportId).getName()+".html";
         log.error("-------------------wyw:开始上传file==================");
-        FileResp.FileResult obj=(FileResp.FileResult)fileService.uploadFile(file.getOriginalFilename(),inputStreamPdf,file.getSize()).getData();
+        FileResp.FileResult obj=(FileResp.FileResult)fileService.uploadFile(name,inputStreamPdf,file.getSize()).getData();
         //更新fileIds
         log.error("-------------------wyw:开始存入fileIds==================");
-        reportService.updateFileId(Long.parseLong(file.getOriginalFilename().replace(".html","")), obj.getFileId());
+        reportService.updateFileId(reportId, obj.getFileId());
         log.error("-------------------wyw:结束存入fileIds=================");
         return obj.getFileId();
     }
